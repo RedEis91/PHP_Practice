0f33a21a2f934c5e4885da1ac9f615eb20ca2df8
RedEis91 <imitationkrabs91@gmail.com>
2018-02-26 15:58:58 -0500

    first commit

---
+++ dynamic_text.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   <?php
+    
+    //reusable variable
+    $title = "Jared Eisenhauer Official Site";
+    
+    ?>
+   
+   
+   <h1><?php
+       //instance of variable, can be reused on multiple pages
+       echo $title; 
+       
+       ?></h1>
+    
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   
+<?php
+    //variables can't start with a number and are case sensitive
+    $name = "Jared";
+    //never enclose numbers in quotes
+    $number = 100;
+    
+    //concatonation is done with "."
+    echo $name . " keep it " . $number;
+        //can reassign variables within document
+    $name = "<h1>Red</h1>";
+    
+    echo $name;
+    
+?>
+   
+   
+    
+</body>
+</html>
